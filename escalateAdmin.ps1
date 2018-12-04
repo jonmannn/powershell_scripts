c417492505dddfc9eca92db0d5d2f8419ad94256
Jonmann <jonmannn@gmail.com>
2018-12-04 11:43:51 -0500

    Fixed improper variale assignment
    Forgot to update variale i was working with

--- escalateAdmin.ps1
+++ escalateAdmin.ps1
@@ -1,6 +1,6 @@
 $logged_in_user = Get-WMIObject -class Win32_ComputerSystem | Select-Object username | Select-String "username"
 $logged_in_user = [string]$logged_in_user
-$logged_in_user.Trim("}").Substring(11)
+$logged_in_user = $logged_in_user.Trim("}").Substring(11)
 
 $group = "Administrators";
 $groupObj =[ADSI]"WinNT://./$group,group" 
