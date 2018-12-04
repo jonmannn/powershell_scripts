21c12d29b8ff69e58f82ba58485907d32d2662e1
Jonmann <jonmannn@gmail.com>
2018-12-04 11:34:26 -0500

    First attempt

---
+++ escalateAdmin.ps1
@@ -0,0 +1,8 @@
+$logged_in_user = Get-WMIObject -class Win32_ComputerSystem | Select-Object username | Select-String "username"
+$logged_in_user = [string]$logged_in_user
+$logged_in_user.Trim("}").Substring(11)
+
+$group = "Administrators";
+$groupObj =[ADSI]"WinNT://./$group,group" 
+$userObj = [ADSI]"WinNT://$logged_in_user,user"
+$groupObj.Add($userObj.Path)
