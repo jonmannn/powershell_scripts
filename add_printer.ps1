7ea831c7756244366e8a1b1b606d45d6dbacbf2e
Jon Mann <jonmannnyc@gmail.com>
2018-12-19 13:50:54 -0500

    added quotes

--- add_printer.ps1
+++ add_printer.ps1
@@ -2,7 +2,7 @@ $printer_ip = "10.122.160.7"
 $portName = "TCPPort:$printer_ip"
 $printDriverName = "PCL6 Driver for Universal Print"
 
-pnputil.exe /a /i C:\Program Files (x86)\AirWatch\Warby\Assets\RicohUniversal\oemsetup.inf
+pnputil.exe /a /i "C:\Program Files (x86)\AirWatch\Warby\Assets\RicohUniversal\oemsetup.inf"
  
 $portExists = Get-Printerport -Name $portname -ErrorAction SilentlyContinue
  
