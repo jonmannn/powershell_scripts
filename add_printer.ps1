0599a47076e80ef2ffcc0e7ed75c7b081cf987fc
Jon Mann <jonmannnyc@gmail.com>
2018-12-28 11:43:18 -0500

    added printer install template

--- add_printer.ps1
+++ add_printer.ps1
@@ -1,8 +1,6 @@
 $printer_ip = "10.10.10.10"
 $portName = "TCPPort:$printer_ip"
-$printDriverName = "PCL6 Driver for Universal Print"
-
-pnputil.exe /add-driver "C:\path\to\oemsetup.inf" /install
+$printDriverName = "PCL6 Driver for Universal Print" # Get this value from the INF file
  
 $portExists = Get-Printerport -Name $portname -ErrorAction SilentlyContinue
  
@@ -13,6 +11,7 @@ if (-not $portExists) {
 $printDriverExists = Get-PrinterDriver -name $printDriverName -ErrorAction SilentlyContinue
  
 if (-not $printDriverExists) {
+    pnputil.exe /add-driver "C:\path\to\oemsetup.inf" /install # Make sure INF is in same directory as referenced files inside it
     Add-PrinterDriver -Name $printDriverName
 }
 
